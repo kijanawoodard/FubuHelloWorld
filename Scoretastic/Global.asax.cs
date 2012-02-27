49274a6e5a1a5693a819f74a556b1fba529b92fa
Kijana Woodard <github@wyldeye.com>
2012-02-26 22:13:14 -0600

    moved conifguration stuff to configuration folder

--- Scoretastic/Global.asax.cs
+++ Scoretastic/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
+using Scoretastic.Configuration;
 
 namespace Scoretastic
 {
@@ -12,7 +13,7 @@ namespace Scoretastic
 
         protected void Application_Start(object sender, EventArgs e)
         {
-
+            StartFubuMvc.Start();
         }
 
         protected void Session_Start(object sender, EventArgs e)
